914d8f2a94dcc0c8a85cbda4ff60c4bef713536f
Mykhailo Kuzich <timmkedth@gmail.com>
2022-12-02 15:10:22 +0200

    add template

---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import { GlobalStyle } from './GlobalStyle';
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+};
---
+++ src/theme.ts
@@ -0,0 +1,60 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    text: '#2a2a2a',
+    background: '#fff',
+    title: '#07c',
+    darkBg: '#5476C4',
+    darkText: '#E8F1FF',
+    darkAccent: '#61C7D4',
+    borders: '#E1ECED',
+    primary: '#07c',
+    secondary: '#05a',
+    accent: '#609',
+    muted: '#f6f6f6',
+  },
+  space: [0, 2, 4, 8, 16, 32, 48, 64, 88, 104, 240],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'system-ui, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '24px',
+    xl: '32px',
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    small: '4px',
+    normal: '8px',
+    large: '16px',
+    round: '50%',
+  },
+  shadows: {
+    main: '0 4px 8px rgba(0, 0, 0, 0.4)',
+    hover: '0 0 32px rgba(0, 0, 0, 0.4)',
+  },
+  transition: {
+    scale: 'scale 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+    transform: 'transform 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+    boxShadow: 'box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+    color: 'color 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+    backgroundColor: 'background-color 250ms cubic-bezier(0.4, 0, 0.2, 1)',
+  },
+};
